420405b38db67047dd138d20d14f411e40b3779a
Kevin Lallier <kevin.lallier@etu.unicaen.fr>
2023-01-19 15:26:09 +0100

    Creation des vues + creation de la classe Element

---
+++ TD1/src/main/java/edu/spring/TD1/models/Element.java
@@ -0,0 +1,29 @@
+package edu.spring.TD1.models;
+
+import java.util.Objects;
+
+public class Element {
+
+    private String nom;
+    private int evaluation;
+
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public int getEvaluation() {
+        return evaluation;
+    }
+    public void setEvaluation(int evaluation) {
+        this.evaluation = evaluation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Element element)) return false;
+        return evaluation == element.evaluation && Objects.equals(nom, element.nom);
+    }
+}
