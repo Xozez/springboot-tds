4ed6906b22ae11e011cde978cccdd6bdcc874741
Kevin Lallier <kevin.lallier@etu.unicaen.fr>
2023-01-19 15:15:07 +0100

    Commit creation du projet TD1

---
+++ TD1/src/main/java/edu/spring/TD1/Td1Application.java
@@ -0,0 +1,13 @@
+package edu.spring.TD1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Td1Application.class, args);
+	}
+
+}
---
+++ TD1/src/test/java/edu/spring/TD1/Td1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.spring.TD1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
