a415e8854d478884e3d4617679519c0b4ec6552c
Kevin Lallier <kevin.lallier@etu.unicaen.fr>
2023-01-27 09:28:18 +0100

    Creation des messages pour les fenetres ajouter,incrementer et decrementer

--- TD1/src/main/java/edu/spring/TD1/MainController.java
+++ TD1/src/main/java/edu/spring/TD1/MainController.java
@@ -2,6 +2,7 @@ package edu.spring.TD1;
 
 import edu.spring.TD1.models.Item;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
@@ -14,34 +15,37 @@ public class MainController {
     public List<Item> getItems(){
         return new ArrayList<>();
     }
-    @GetMapping("/new")
+    @GetMapping("/")
     public RedirectView newItem(){
         // TODO
         return new RedirectView("/items/new.html");
     }
 
     @PostMapping("/addNew")
-    public RedirectView addNew(@SessionAttribute("items") List<Item> items,@RequestParam String nom) {
+    public RedirectView addNew(@SessionAttribute("items") List<Item> items,@RequestParam String nom, RedirectAttributes attrs) {
         Item element = new Item(nom,0);
         items.add(element);
-        return new RedirectView("/items/");
+        attrs.addFlashAttribute("msg", nom + " a été décrémenté" );
+        return new RedirectView("/");
     }
     @PostMapping("/inc/{nom}")
-    public RedirectView inc(@SessionAttribute("items") List<Item> items,@PathVariable("nom") String nom) {
+    public RedirectView inc(@SessionAttribute("items") List<Item> items,@PathVariable("nom") String nom, RedirectAttributes attrs){
         for (Item element : items) {
             if (element.getNom().equals(nom)) {
                 element.setEvaluation(element.getEvaluation() + 1);
             }
         }
-        return new RedirectView("/items/");
+        attrs.addFlashAttribute("msg", nom + " a été incrémenté" );
+        return new RedirectView("/");
     }
     @PostMapping("/dec/{nom}")
-    public RedirectView dec(@SessionAttribute("items") List<Item> items,@PathVariable("nom") String nom) {
+    public RedirectView dec(@SessionAttribute("items") List<Item> items, @PathVariable("nom") String nom, RedirectAttributes attrs) {
         for (Item element : items) {
             if (element.getNom().equals(nom)) {
                 element.setEvaluation(element.getEvaluation() - 1);
             }
         }
-        return new RedirectView("/items/");
+        attrs.addFlashAttribute("msg", nom + " a été décrémenté" );
+        return new RedirectView("/");
     }
 }
